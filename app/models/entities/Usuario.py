f5a5f56e8daa59082bb37890185c90c759d27ac9
oswaldo Echeverria <oswaldo.echeverria@outlook.com>
2023-11-10 18:09:05 -0500

    Logica para encriptar y verificar contrasenia en la Eentidad usuarios entities/Usuario.py

--- app/models/entities/Usuario.py
+++ app/models/entities/Usuario.py
@@ -1,6 +1,14 @@
+from werkzeug.security import generate_password_hash, check_password_hash
+
+
 class Usuario:
     def __init__(self, id, usuario, password, tipo_usuario):
         self.id = id
         self.usuario = usuario
         self.password = password
         self.tipo_usuario = tipo_usuario
+
+    def encriptar_password(password):
+        encriptado = generate_password_hash(password)
+        coincide = check_password_hash(encriptado, password)
+        return coincide
--- config.py
+++ config.py
@@ -1,15 +1,14 @@
 class Config:
-    SECRET_KEY='B!1weNAt1t^%kvhUI*S^'
+    SECRET_KEY = "B!1weNAt1t^%kvhUI*S^"
+
 
 class DevelopmentConfig(Config):
-    DEBUG =True
+    DEBUG = True
     # Configuracion Mysql
-    MYSQL_HOST = 'localhost'
-    MYSQL_USER = 'root'
-    MYSQL_PASSWORD = 'abc123....'
-    MYSQL_DB = 'tienda'
+    MYSQL_HOST = "localhost"
+    MYSQL_USER = "root"
+    MYSQL_PASSWORD = "abc123...."
+    MYSQL_DB = "tienda"
 
-config = {
-    'development': DevelopmentConfig,
-    'default': DevelopmentConfig
-}
+
+config = {"development": DevelopmentConfig, "default": DevelopmentConfig}
